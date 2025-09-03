e3f8d8d1ca06fdcab35746b4d3622d05876a1b08
PeawZaZa <pawaris2020@gmail.com>
2025-09-03 10:24:27 +0700

    library testing

--- server.js
+++ server.js
@@ -1,14 +1,47 @@
-// server.js - My First Express Server
+// server.js
 const express = require('express');
-const app = express();
-const PORT = 3000;
+require('dotenv').config(); // << เพิ่มบรรทัดนี้ที่ด้านบน
 
-// สร้าง Route (เส้นทาง) แรกสำหรับหน้าหลัก
-app.get('/', (req, res) => {
-  res.send('<h1>Hello from ENGSE203 Express Server!</h1>');
+const app = express();
+const PORT = process.env.PORT || 3000; // << อ่านค่า PORT จาก .env
+const APP_NAME = process.env.APP_NAME;
+const cors = require('cors'); // << Import cors
+require('dotenv').config();
+const helmet = require('helmet');
+const Joi = require('joi'); // << Import Joi
+
+
+app.use(helmet());
+app.use(cors()); 
+app.use(express.json()); // << สำคัญ! ต้องมี middleware นี้เพื่ออ่าน JSON body
+
+const userSchema = Joi.object({
+    username: Joi.string().alphanum().min(3).max(30).required(),
+    password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
+    birth_year: Joi.number().integer().min(1900).max(new Date().getFullYear())
 });
 
-// เริ่มต้นให้ Server รอรับการเชื่อมต่อ
+// Route สำหรับสร้าง user
+app.post('/api/users', (req, res) => {
+    const { error, value } = userSchema.validate(req.body);
+
+    if (error) {
+        // ถ้าข้อมูลไม่ถูกต้อง ส่ง 400 Bad Request กลับไปพร้อมรายละเอียด
+        return res.status(400).json({ message: 'Invalid data', details: error.details });
+    }
+
+    // ถ้าข้อมูลถูกต้อง
+    console.log('Validated data:', value);
+    res.status(201).json({ message: 'User created successsssfully!', data: value });
+});
+app.get('/', (req, res) => {
+  res.send(`<h1>Hello from ${APP_NAME}!ss</h1>`);
+});
 app.listen(PORT, () => {
-  console.log(`🚀 Server is running on http://localhost:${PORT}`);
+  console.log(`🚀 ${APP_NAME} is running on http://localhost:${PORT}`);
+});
+
+
+app.get('/api/data', (req, res) => {
+    res.json({ message: 'This data is open for everyone!' });
 });
